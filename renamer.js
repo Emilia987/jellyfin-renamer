cf15789e18a3bd8acb1fbe756a81270431e63eac
Emily <133243828+Emily-SAC@users.noreply.github.com>
2023-12-03 16:52:02 +0100

    renamer.js
    fixed naming order and illegal windows characters

--- renamer.js
+++ renamer.js
@@ -11,11 +11,15 @@ rl.question('What is the name of the show? ', (showName) => {
   rl.question('What is the season number? ', (seasonNumber) => {
     rl.question('What is the year of the show? ', (year) => {
       const baseName = 'EP.';
-      const dirPath = 'C:/Users/***/Downloads/Jellyfin renamer';
+      const dirPath = 'C:/Users/Emily/Downloads/Jellyfin renamer';
       const files = fs.readdirSync(dirPath)
-                    .filter(file => file.startsWith(baseName));
-      
-      // Create the show folder
+                    .filter(file => file.startsWith(baseName))
+                    .sort((a, b) => {
+                      const episodeA = parseInt(a.match(/\d+/)[0]);
+                      const episodeB = parseInt(b.match(/\d+/)[0]);
+                      return episodeA - episodeB;
+                    });
+
       const showFolderName = `${showName} (${year})`;
       const desktopPath = require('path').join(require('os').homedir(), 'Desktop');
       const showFolderPath = path.join(desktopPath, showFolderName);
@@ -23,7 +27,6 @@ rl.question('What is the name of the show? ', (showName) => {
         fs.mkdirSync(showFolderPath);
       }
 
-      // Create the season folder and move/renamed files into it
       const seasonFolderName = `Season ${seasonNumber.padStart(2, '0')}`;
       const seasonFolderPath = path.join(showFolderPath, seasonFolderName);
       if (!fs.existsSync(seasonFolderPath)) {
@@ -34,14 +37,15 @@ rl.question('What is the name of the show? ', (showName) => {
       const renameNextFile = () => {
         if (i < files.length) {
           const file = files[i];
-          const episodeNumber = file.match(/\d+/)[0];
+          const episodeNumber = parseInt(file.match(/\d+/)[0]);
           const paddedEpisodeNumber = episodeNumber.toString().padStart(2, '0');
-          const episodeName = `Episode ${i+1}`;
+          const episodeName = `Episode ${episodeNumber}`;
           const fileName = `${episodeName} S${seasonNumber.padStart(2, '0')}E${paddedEpisodeNumber}`;
           const oldPath = path.join(dirPath, file);
           const newPath = path.join(seasonFolderPath, `${fileName}${path.extname(file)}`);
           rl.question(`What is the name of ${fileName}? `, (name) => {
-            const newName = `${name} S${seasonNumber.padStart(2, '0')}E${paddedEpisodeNumber}`;
+            const cleanName = name.replace(/[<>:"\/\\|?*]/g, '');
+            const newName = `${cleanName} S${seasonNumber.padStart(2, '0')}E${paddedEpisodeNumber}`;
             fs.renameSync(oldPath, path.join(seasonFolderPath, `${newName}${path.extname(file)}`));
             console.log(`File renamed to ${newName}${path.extname(file)}`);
             i++;
